a5078401f52a3b3c85d8be07718838146ee2a294
Servandofg12 <servandofg12@gmail.com>
2021-12-01 20:13:48 +0100

    Titulo: Integrar la nueva interfaz de usuario con el nuevo formulario de sugerencia. Mensaje: Se ha intentado hacer un formulario de login y hacer un logout
    Que queda por hacer: Hacer de manera correcta el login y logout
    
    ID: SF-01

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from .views import BoothView, SugerenciaVista, Inicio, send_suggesting_form
+from django.urls import path, include
+from .views import BoothView, SugerenciaVista, Inicio, send_suggesting_form, LogoutView, LoginView, loginformpost
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import logout, login
+from rest_framework.authtoken.views import obtain_auth_token
+
+
 
 
 urlpatterns = [
-    path('', Inicio.get_pagina_inicio, name="pagina-inicio"),
+    path('', LoginView.as_view(), name="login"),
+    path('logout/', logout, name="logout"),
+    path('login/', loginformpost, name="login-send"),
+    #path('inicio/', Inicio.get_pagina_inicio, name="pagina-inicio"),
     path('<int:voting_id>/', BoothView.as_view(), name="votacion"),
     path('sugerenciaformulario/', SugerenciaVista.sugerencia_de_voto),
     path('sugerenciaformulario/send/', send_suggesting_form, name="suggesting-send")
--- decide/booth/views.py
+++ decide/booth/views.py
@@ -18,16 +18,96 @@ from django.template import RequestContext
 from booth.models import Sugerencia
 
 from census.models import Census
+from voting.models import Voting
+from django.contrib.auth.models import User
+
+
+class LoginView(TemplateView):
+    template_name = 'booth/login.html'
+
+class LogoutView(TemplateView):
+    template_name = 'booth/inicio.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        token = self.request.session.get('user_token')
+        if token:
+            mods.post('authentication', entry_point='/logout/', json={'token':token})
+            del self.request.session['user_token']
+            del self.request.session['voter_id']
+            del self.request.session['username']
+
+        return context
+    
+    def render_to_response(self, context, **response_kwargs):
+        response = super(LogoutView, self).render_to_response(context, **response_kwargs)
+        response.delete_cookie('decide')
+        return response
+
+
+def autenticacion(request, username, password):
+    token= mods.post('authentication', entry_point='/login/', json={'username':username, 'password':password})
+    request.session['user_token']=token
+    voter = mods.post('authentication', entry_point='/getuser/', json=token)
+    voter_id = voter.get('id', None)
+    request.session['voter_id'] = voter_id
+
+    if voter_id == None:
+        return False, voter_id
+
+    return True, voter_id
+
+def get_user(self):
+    token = self.request.session.get('user_token', None)
+    voter = mods.post('authentication', entry_point='/getuser/', json=token)
+    voter_id = voter.get('id', None)
+    return json.dumps(token.get('token', None)), json.dumps(voter), voter_id
+
+def loginformpost(request):
+        if request.method == 'POST':
+
+            username = request.POST['username']
+            request.session['username'] = username
+            password = request.POST['password']
+
+            # Autenticacion
+            voter, voter_id = autenticacion(request, username, password)
+
+            if not voter:
+                return render(request, 'booth/login.html', {'no_user':True})
+            else:
+                vista = get_pagina_inicio(request, voter_id)
+                return vista
+                #return HttpResponseRedirect(reverse('pagina-inicio'))
+        else:
+            return render(request, 'booth/login.html', {'no_user':True})
+
+
+def get_pagina_inicio(request, voter_id):
+
+    print("HA ENTRADO AQUI")
+    template = 'booth/inicio.html'
+    user_actual = User.objects.all().filter(id=voter_id)[0]
+    print(request.user)
+    usuario_valido = User.objects.all().filter(id=user_actual.id).count()
+    num_censos_votante_actual = Census.objects.all().filter(voter_id=user_actual.id).count()
+    censos_votante_actual = Census.objects.all().filter(voter_id=user_actual.id)
+    return render(request, template, {"censos": censos_votante_actual, "num_censos":num_censos_votante_actual,
+            "user":user_actual, "usuario_valido": usuario_valido})
 
 class Inicio(TemplateView):
     template_name = '/booth/inicio.html'
 
     def get_pagina_inicio(request):
+
         template = 'booth/inicio.html'
         user_actual = request.user
+        usuario_valido = User.objects.all().filter(id=user_actual.id).count()
         num_censos_votante_actual = Census.objects.all().filter(voter_id=user_actual.id).count()
         censos_votante_actual = Census.objects.all().filter(voter_id=user_actual.id)
-        return render(request, template, {"censos": censos_votante_actual, "num_censos":num_censos_votante_actual, "user":user_actual})
+        return render(request, template, {"censos": censos_votante_actual, "num_censos":num_censos_votante_actual,
+        "user":user_actual, "usuario_valido": usuario_valido})
+        
 
 
 # TODO: check permissions and census
