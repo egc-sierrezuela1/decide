b5f9cb05f09fbeeffa723d0166a66d77a3ce85d7
Servandofg12 <servandofg12@gmail.com>
2021-12-16 14:04:24 +0100

    Titulo: SOPORTE MULTIPLES PREGUNTAS Mensaje: Realizar test para soporte de multiples preguntas.
    Que queda por hacer: Queda por integrarlo con el modulo de Voting.
    
    ID: SF-01

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -15,27 +15,96 @@ class PostProcTestCase(APITestCase):
     def tearDown(self):
         self.client = None
 
+
     def test_identity(self):
-        data = {
+        data = [{
             'type': 'IDENTITY',
             'options': [
-                { 'option': 'Option 1', 'number': 1, 'votes': 5 },
-                { 'option': 'Option 2', 'number': 2, 'votes': 0 },
-                { 'option': 'Option 3', 'number': 3, 'votes': 3 },
-                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
-                { 'option': 'Option 5', 'number': 5, 'votes': 5 },
-                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
+                {'option': 'Option 1', 'number': 1, 'votes': 5},
+                {'option': 'Option 2', 'number': 2, 'votes': 0},
+                {'option': 'Option 3', 'number': 3, 'votes': 3},
+                {'option': 'Option 4', 'number': 4, 'votes': 2},
+                {'option': 'Option 5', 'number': 5, 'votes': 5},
+                {'option': 'Option 6', 'number': 6, 'votes': 1},
             ]
-        }
+        }]
 
-        expected_result = [
-            { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 5 },
-            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 5 },
-            { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 3 },
-            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 2 },
-            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 1 },
-            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
-        ]
+        expected_result = [{
+            'type': 'IDENTITY',
+            'options': [
+                {'option': 'Option 1', 'number': 1, 'votes': 5,
+                 'postproc': 5},
+                {'option': 'Option 5', 'number': 5, 'votes': 5,
+                 'postproc': 5},
+                {'option': 'Option 3', 'number': 3, 'votes': 3,
+                 'postproc': 3},
+                {'option': 'Option 4', 'number': 4, 'votes': 2,
+                 'postproc': 2},
+                {'option': 'Option 6', 'number': 6, 'votes': 1,
+                 'postproc': 1},
+                {'option': 'Option 2', 'number': 2, 'votes': 0,
+                 'postproc': 0},
+            ]
+        }]
+
+        print("VALUE:")
+        print(data)
+        print("EXPECTED_VALUE:")
+        print(expected_result)
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+
+
+    #TEST PARA MULTIPLES PREGUNTAS--De Guadalentin
+    def test_identity_multiple_questions(self):
+        data = [{
+            'type': 'IDENTITY',
+            'options': [
+                {'option': 'Option 1', 'number': 1, 'votes': 5},
+                {'option': 'Option 2', 'number': 2, 'votes': 0},
+                {'option': 'Option 3', 'number': 3, 'votes': 3}
+            ]
+        }, {
+            'type': 'IDENTITY',
+            'options': [
+                {'option': 'Option 1', 'number': 1, 'votes': 2},
+                {'option': 'Option 2', 'number': 2, 'votes': 5},
+                {'option': 'Option 3', 'number': 3, 'votes': 1}
+            ]
+        }]
+
+        #El orden debe ser descendente, según el serializer
+        expected_result = [{
+            'type': 'IDENTITY',
+            'options': [
+                {'option': 'Option 1', 'number': 1, 'votes': 5,
+                 'postproc': 5},
+                {'option': 'Option 3', 'number': 3, 'votes': 3,
+                 'postproc': 3},
+                {'option': 'Option 2', 'number': 2, 'votes': 0,
+                 'postproc': 0}
+            ]
+        }, {
+            'type': 'IDENTITY',
+            'options': [
+                {'option': 'Option 2', 'number': 2, 'votes': 5,
+                 'postproc': 5},
+                {'option': 'Option 1', 'number': 1, 'votes': 2,
+                 'postproc': 2},
+                {'option': 'Option 3', 'number': 3, 'votes': 1,
+                 'postproc': 1}
+            ]
+        }]
+
+        print("VALUE:")
+        print(data)
+        print("EXPECTED_VALUE:")
+        print(expected_result)
 
         response = self.client.post('/postproc/', data, format='json')
         self.assertEqual(response.status_code, 200)
--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -14,8 +14,9 @@ class PostProcView(APIView):
             });
 
         out.sort(key=lambda x: -x['postproc'])
-        return Response(out)
+        return out
 
+    
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -29,10 +30,30 @@ class PostProcView(APIView):
            ]
         """
 
-        t = request.data.get('type', 'IDENTITY')
-        opts = request.data.get('options', [])
+        out = []
+        questions = request.data
 
-        if t == 'IDENTITY':
-            return self.identity(opts)
+        for q in questions:
+            result = None
+            t = q['type']
+            opts = q['options']
 
-        return Response({})
+            if t == 'IDENTITY':
+                result = self.identity(opts)
+            '''if t == 'BORDA':
+                result = self.borda(opts)
+            if t == 'EQUALITY':
+                result = self.equality(opts)
+            if t == 'SAINTE_LAGUE' or t == 'HONDT':
+                result = self.proportional_representation(opts, t)
+            if t == 'DROOP':
+                result = self.droop(opts)
+            if t == 'IMPERIALI':
+                result = self.imperiali(opts)
+            if t == 'HARE':
+                result = self.hare(opts)'''
+
+            out.append({'type': t, 'options': result})
+
+
+        return Response(out)
