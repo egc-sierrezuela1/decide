896e3bc271532012cfa568272f234ecaedb40c0d
serfiggom <72518706+Servandofg12@users.noreply.github.com>
2022-01-03 14:52:27 +0100

    Update settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -185,6 +185,6 @@ INSTALLED_APPS = INSTALLED_APPS + MODULES
 
 
 NOSE_ARGS = ['--with-xunit']
-'''import django_heroku
-django_heroku.settings(locals(),test_runner=False)'''
+import django_heroku
+django_heroku.settings(locals(),test_runner=False)
 
